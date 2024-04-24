1b4afedc8ec8bae90599bb8c45bb46416c5ec49f
jacindaevans <jacindaevans.je@gmail.com>
2024-04-24 09:38:52 -0400

    first commit and push

---
+++ Homework2.py
@@ -0,0 +1,17 @@
+#Homework 1: Create a dictionary and upload it to Github
+
+thisdict = {
+"brand": "Toyota",
+"model": "Highlander",
+"year": 2024
+}
+
+thatdict = {
+"place": "Chickfila",
+"meal": "numberonecombo",
+"drink": "lemonadenoice",
+"price": 9
+}
+
+print(thisdict)
+print(thatdict)
---
+++ main.py
@@ -0,0 +1,43 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print('Hello world')
+Jacindaslist= 1,2,3,4,5,6,7,8,9,10
+for even_numbers in Jacindaslist(1,10,2):
+    #for this I used the Forloop
+#The numbers inside the colon represent the starting number, ending number, interval aka divisible by 2
+    print(even_numbers,end=' ')
+Jacindaslist=[1,2,3,4,5,6,7,8,9,10]
+for even_numbers in Jacindaslist(1,10,2):
+    print(even_numbers,end=' ')
+for even_numbers in Jacindaslist(1,10,2):
+    print(even_numbers,end=' ')
+
+Jacindaslist= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#extract only even numbers --homework
+for even_numbers in Jacindaslist(1,10,2):
+    print(even_numbers,end=' ')
+Jacindaslist= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for num in Jacindaslist(1, 11):
+    if num % 2 == 0:
+        print (num, end=' ')
+
+#Homework 1: Create a dictionary and upload it to Github
+
+thisdict = {
+"brand": "Toyota",
+"model": "Highlander",
+"year": 2024
+}
+
+thatdict = {
+"place": "Chickfila",
+"meal": "numberonecombo",
+"drink": "lemonadenoice",
+"price": 9
+}
+
+print(thisdict)
