98e054c25fa73b456d92e67db755ff51d0bf668b
jacindaevans <jacindaevans.je@gmail.com>
2024-05-22 07:31:19 -0400

    Functions Calling functions w/in functions

---
+++ Homework4.py
@@ -0,0 +1,22 @@
+#create a function that returns my first and last name, create a function that returns my age, create a function that returns both
+
+def fullname ():
+    print("Jacinda Evans")
+
+fullname()
+
+def age():
+    print(33)
+
+age()
+
+def fullname_age():
+    demo = (fullname + ", " + age)
+    return fullname_age()
+
+print (fullname_age)
+
+
+
+
+
